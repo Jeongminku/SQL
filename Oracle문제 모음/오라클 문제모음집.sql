eab1aca052ef818c2aad17d3cf61a8a64517b413
Jeongminku <jmk00140@gmail.com>
2022-12-13 15:14:27 +0900

    up

--- Oracle문제 모음/오라클 문제모음집.sql
+++ Oracle문제 모음/오라클 문제모음집.sql
@@ -87,38 +87,38 @@ select employee_id, last_name || ' ' || first_name, job_id, salary from employee
 
 --==========================================================================================================================--
 -- ■ 1. emp 테이블에서 사원번호가 7698 인 사원의 이름, 업무, 급여를 출력하시오. ■
-select ename, job, sal from emp
+select ename, job, sal , mgr "사원번호" from emp where mgr = '7698';
 
 -- ■ 2. emp 테이블에서 사원이름이 SMITH인 사람의 이름과 월급, 부서번호를 구하시오. ■
-
+select ename, sal, deptno from emp where ename = 'SMITH';
 -- ■ 3. 월급이 2500이상 3500미만인 사원의 이름, 입사일, 월급을 구하시오. ■
-
+select ename, hiredate, sal from emp where sal between 2500 and 3500;
 -- ■ 4. 급여가 2000에서 3000사이에 포함되지 않는 사원의 이름, 업무, 급여를 출력하시오. ■
-
+select ename, job, sal from emp where sal not between 2000 and 3000;
 -- ■ 5. 81년05월01일과 81년12월03일 사이에 입사한 사원의 이름, 급여, 입사일을 출력하시오. ■
-
+select ename, sal, hiredate from emp where hiredate between '81/05/01' and '81/12/03';
 -- ■ 6. emp테이블에서 사원번호가 7566,7782,7934인 사원을 제외한 사람들의 사원번호,이름, 월급을 출력하시오.  ■
-
+select empno, ename, sal from emp where empno not in (7566,7782,7934);
 -- ■ 7. 부서번호 30(deptno)에서 근무하며 월2,000달러 이하를 받는 81년05월01일 이전에 입사한 사원의 이름, 급여, 부서번호, 입사일을 출력하시오. ■
-
+select ename, sal , deptno, hiredate from emp where deptno = 30 and sal < 2000;
 -- ■ 8. emp테이블에서 급여가 2,000와 5,000 사이고 부서번호가 10 또는 30인 사원의 이름과 급여,부서번호를 나열하시오. ■
-
+select ename, sal, deptno from emp where sal between 2000 and 5000 and (deptno = 10 or deptno = 30);
 -- ■ 9. 업무가 SALESMAN 또는 MANAGER이면서 급여가 1,600, 2,975 또는 2,850이 아닌 모든 사원의 이름, 업무 및 급여를 표시하시오. ■
-
+select ename, job, sal from emp where (job = 'SALESMAN' or JOB = 'MANAGER') and sal not in (1600, 2975,2850);
 --10없음.
 
 -- ■ 11. emp테이블에서 이름에 A와 E가 있는 모든 사원의 이름을 표시하시오. ■
-
+select ename from emp where ename like ('%A%') and ename like ('%E%'); 
 -- ■ 12. emp테이블에서 관리자가 없는 모든 사원의 이름과 업무를 표시하시오. ■
-
+select ename, job from emp where job != 'MANAGER';
 -- ■ 13. emp테이블에서 커미션 항목이 입력된 사원들의 이름과 급여, 커미션을 구하시오. ■
-
+select ename, sal, comm from emp where comm is not null;
 --==========================================================================================================================--
 
 -- ■ 1. EMPLOYEES 테이블에서 입사일자 순으로 정렬하여 사원번호, 이름, 업무, 급여, 입사일자, 부서번호를 출력하라. ■
-
+select employee_id, first_name || ' ' || last_name, job_id, hire_date, department_id from employees order by hire_date asc;
 -- ■ 2. EMPLOYEES 테이블에서 가장 최근에 입사한 순으로 사원번호, 이름, 업무, 급여, 입사일자, 부서번호를 출력하라. ■
-
+select * from employees;
 -- ■ 3. EMPLOYEES 테이블에서 부서번호로 정렬한 후 부서번호가 같을 경우 급여가 많은 순으로 정렬하여 사원번호, 성명, 업무, 부서번호, 급여를 출력하여라. ■
 
 -- ■ 4. EMPLOYEES 테이블에서 첫번째 정렬은 부서번호로 두번째 정렬은 업무로 세번째정렬은 급여가 많은 순으로 정렬하여 사원번호, 성명, 입사일자, 부서번호, 업무, 급여를 출력하여라. ■
